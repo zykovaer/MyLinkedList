05971c093b0f727c48fdf870bdac4569c640fe19
YuriSilenok <yuri.silenok@gmail.com>
2023-05-19 18:56:38 +0300

    Добавьте файлы проекта.

---
+++ MyLinkedList/MyLinkedList.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedList
+{
+    internal class MyLinkedList<T> : IEnumerable<T>
+    {
+        class Item
+        {
+            public T Value;
+            public Item Next;
+        }
+
+        private Item Head;
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Item item = Head;
+            while (item != null)
+            {
+                yield return item.Value;
+                item = item.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override string ToString()
+        {
+            return String.Join(" ", this);
+        }
+
+        public void AddLast(T value)
+        {
+            Item item = new Item() { Value = value};
+            if (Head == null)
+            {
+                Head = item;
+                return;
+            }
+
+            Item current = Head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = item;
+            
+        }
+
+        public void AddFirst(T value)
+        {
+            Item item = new Item() { Value = value };
+            item.Next = Head;
+            Head = item;
+        }
+
+        public bool Contains(T value)
+        {
+            Item current = Head;
+
+            while (current != null)
+            {
+                if (current.Value.Equals(value)) return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public T Search(int i)
+        {
+            if (Head == null) throw new NullReferenceException("Список пуст");
+
+            Item current = Head;
+            int index = 0;
+            while (current != null)
+            {
+                if (index == i)
+                    return current.Value;
+                current = current.Next;
+                index++;
+                
+            }
+            throw new Exception("Индекс находятся за пределами списка");
+        }
+        public int Count
+        {
+            get
+            {
+                if (Head == null)
+                {
+                    return 0;
+                }
+                else
+                {
+                    int count = 0;
+                    Item current = Head;
+                    while (current != null)
+                    {
+                        count++;
+                        current = current.Next;
+                    }
+                    return count;
+                }
+            }
+        }
+        public void AddBefore(T value, int index)
+        {
+            if (index >= this.Count || index < 0)
+            {
+                throw new IndexOutOfRangeException("Индекс находился вне границ списка");
+            }
+
+            if (index == 0)
+            {
+                this.AddFirst(value);
+            }
+            else
+            {
+                Item newEl = new Item() { Value = value };
+
+                Item tmp = Head;
+                Item prev = tmp;
+                for (int i = 1; i <= index; i++)
+                {
+                    prev = tmp;
+                    tmp = tmp.Next;
+                }
+
+                newEl.Next = tmp;
+                prev.Next = newEl;
+            }
+        }
+
+        public void AddAfter(T value, int index)
+        {
+            if (index >= this.Count || index < 0)
+            {
+                throw new IndexOutOfRangeException("Индекс находился вне границ списка");
+            }
+
+            Item newEl = new Item() { Value = value };
+
+            Item tmp = Head;
+            Item next = tmp.Next;
+            for (int i = 1; i <= index; i++)
+            {
+                tmp = tmp.Next;
+                next = tmp.Next;
+            }
+
+            tmp.Next = newEl;
+            newEl.Next = next;
+        }
+
+        public T PopFirst()
+        {
+            if (Head == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                T c = Head.Value;
+                Head = Head.Next;
+                return c;
+            }
+        }
+
+        public T PopBack()
+        {
+            if (Head == null)
+            {
+                return default(T);
+            }
+            else
+            {
+                if (Head.Next == null)
+                {
+                    T c = Head.Value;
+                    Head = null;
+                    return c;
+                }
+                else
+                {
+                    Item tmp = Head;
+                    while (tmp.Next.Next != null)
+                    {
+                        tmp = tmp.Next;
+                    }
+                    T c = tmp.Next.Value;
+                    tmp.Next = null;
+                    return c;
+                }
+            }
+        }
+
+        public T PopAfter(int index)
+        {
+            if (index >= this.Count - 1 || index < 0)
+            {
+                throw new IndexOutOfRangeException("Индекс находился вне границ списка");
+            }
+
+            Item tmp = Head;
+            int count = 0;
+            while (count != index)
+            {
+                tmp = tmp.Next;
+                count++;
+            }
+            T result = tmp.Value;
+            tmp.Next = tmp.Next.Next;
+            return result;
+        }
+        public T PopBefore(int index)
+        {
+            if (index >= this.Count || index < 1)
+            {
+                throw new IndexOutOfRangeException("Индекс находился вне границ списка");
+            }
+
+            if (index == 1)
+            {
+                return PopFirst();
+            }
+            else
+            {
+                Item tmp = Head;
+                int count = 0;
+                while (count != index - 2)
+                {
+                    tmp = tmp.Next;
+                    count++;
+                }
+                T result = tmp.Value;
+                tmp.Next = tmp.Next.Next;
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// Вставить после каждого элемента, занимающего четную позицию в списке, 
+        /// новый элемент Е1. Первый элемент имеет значение позиции равный 0
+        /// </summary>
+        /// <param name="value"></param>
+        public void InsertAfterEven(T value)
+        {
+            if (Head == null) return;
+
+            Item current = Head;
+            int index = 0;
+            while (current!=null)
+            {
+                if(index % 2 == 0)
+                {
+                    Item item = new Item() { Value = value };
+                    item.Next = current.Next;
+                    current.Next = item;
+                    current = current.Next;
+                }
+                current = current.Next;
+                index++;
+            }
+        }
+
+        public static MyLinkedList<T> operator +(MyLinkedList<T> myLinkedList1, MyLinkedList<T> myLinkedList2)
+        {
+            if (myLinkedList1.Count != myLinkedList2.Count) throw new ArgumentException("Списки должны быть одинаковой длинны");
+
+            MyLinkedList<T> result = new MyLinkedList<T>();
+            for (int i = 0; i < myLinkedList1.Count; i++)
+            {
+                result.AddLast(myLinkedList1.Search(i));
+                result.AddLast(myLinkedList2.Search(i));
+            }
+            return result;
+        }
+    }
+}
---
+++ MyLinkedList/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo K;
+            MyLinkedList<int> myLinkedList = new MyLinkedList<int>();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("1.Добавление элемента в начало списка");
+                Console.WriteLine("2.Добавление элемента в конец списка");
+                Console.WriteLine("3.Вывод списка на экран");
+                Console.WriteLine("4.Поиск элемента по его значению(возвращает bool)");
+                Console.WriteLine("5.Поиск элемента по номеру(возвращает значение)");
+                Console.WriteLine("6.Добавление элемента перед заданным");
+                Console.WriteLine("7.Добавление элемента после заданного");
+                Console.WriteLine("8.Удаление элемента из начала списка");
+                Console.WriteLine("9.Удаление элемента из конца списка");
+                Console.WriteLine("0.Удаление элемента перед заданным");
+                Console.WriteLine("A.Удаление элемента после заданного");
+                Console.WriteLine("S.Вставить после каждого элемента, занимающего четную позицию в списке, новый элемент Е1.\n\tПервый элемент имеет значение позиции равный 0");
+                Console.WriteLine("D.Создать новый список L, содержащий элементы списков L1 и L2, \n\tчередующиеся между собой (если списки L1 и L2 одинаковой длины)");
+                Console.WriteLine("Esc. Выход из программы");
+                Console.WriteLine(myLinkedList);
+                K = Console.ReadKey();
+                Console.WriteLine();
+                try
+                {
+                    switch (K.Key)
+                    {
+                        case ConsoleKey.D1:
+                        case ConsoleKey.NumPad1:
+                            myLinkedList.AddFirst(Inpiut("Введите значение: "));
+                            break;
+                        case ConsoleKey.D2:
+                        case ConsoleKey.NumPad2:
+                            myLinkedList.AddLast(Inpiut("Введите значение: "));
+                            break;
+                        case ConsoleKey.D3:
+                        case ConsoleKey.NumPad3:
+                            Console.WriteLine(myLinkedList);
+                            break;
+                        case ConsoleKey.D4:
+                        case ConsoleKey.NumPad4:
+                            Console.WriteLine(
+                                myLinkedList.Contains(
+                                    Inpiut("Введите искомое значение: ")));
+                            break;
+                        case ConsoleKey.D5:
+                        case ConsoleKey.NumPad5:
+                            Console.WriteLine(
+                                myLinkedList.Search(
+                                    Inpiut("Введите номер значения: ")));
+                            break;
+                        case ConsoleKey.D6:
+                        case ConsoleKey.NumPad6:
+                            myLinkedList.AddBefore(
+                                Inpiut("Введите значение: "),
+                                Inpiut("Введите индекс: "));
+                            break;
+                        case ConsoleKey.D7:
+                        case ConsoleKey.NumPad7:
+                            myLinkedList.AddAfter(
+                                Inpiut("Введите значение: "),
+                                Inpiut("Введите индекс: "));
+                            break;
+                        case ConsoleKey.D8:
+                        case ConsoleKey.NumPad8:
+                            Console.WriteLine(
+                                myLinkedList.PopFirst());
+                            break;
+                        case ConsoleKey.D9:
+                        case ConsoleKey.NumPad9:
+                            Console.WriteLine(
+                                myLinkedList.PopBack());
+                            break;
+                        case ConsoleKey.D0:
+                        case ConsoleKey.NumPad0:
+                            Console.WriteLine(
+                                myLinkedList.PopBefore(
+                                    Inpiut("Введите индекс: ")));
+                            break;
+                        case ConsoleKey.A:
+                            Console.WriteLine(
+                                myLinkedList.PopAfter(
+                                    Inpiut("Введите индекс: ")));
+                            break;
+                        case ConsoleKey.S:
+                            myLinkedList.InsertAfterEven(
+                                Inpiut("Введите значение: "));
+                            break;
+                        case ConsoleKey.D:
+                            Console.WriteLine("Введите элементы первого списка через пробел");
+                            MyLinkedList<int> myLinkedList1 = new MyLinkedList<int>();
+                            foreach (string item in Console.ReadLine().Split(' '))
+                            {
+                                myLinkedList1.AddLast(int.Parse(item));
+                            }
+
+                            Console.WriteLine("Введите элементы второго списка через пробел");
+                            MyLinkedList<int> myLinkedList2 = new MyLinkedList<int>();
+                            foreach (string item in Console.ReadLine().Split(' '))
+                            {
+                                myLinkedList2.AddLast(int.Parse(item));
+                            }
+
+                            myLinkedList = myLinkedList1 + myLinkedList2;
+                            break;
+                        case ConsoleKey.F:
+                            break;
+                        case ConsoleKey.G:
+                            break;
+                        case ConsoleKey.Escape:
+                            Environment.Exit(0);
+                            break;
+                        default: Console.WriteLine("Request not processed"); break;
+                    }
+                    System.Threading.Thread.Sleep(2000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    System.Threading.Thread.Sleep(2000);
+                }
+            }
+            while (K.Key != ConsoleKey.Escape);
+
+        }
+        static int Inpiut(string message)
+        {
+            int N = 0;
+            while (true)
+            {
+                try
+                {
+                    Console.Write(message);
+                    N = int.Parse(Console.ReadLine());
+
+                    if (N < 1) throw new Exception("Такого не может быть!!!\n");
+                    return N;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
